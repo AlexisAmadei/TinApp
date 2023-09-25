c89ed5aa83482d071af28ff481dcfcefbc8b364b
Alexis Amadei <alexisamadei@gmail.com>
2023-09-26 00:38:36 +0300

    [EDIT] edit path to include apps routes

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -4,15 +4,19 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from "../.config/firebaseConfig";
 
+import Loading from '../components/Loading'
+
 import Landing from './Landing';
 import Terrier from './Terrier';
-import Loading from '../components/Loading'
-import SecurityLayout from '../Layout/SecurityLayout';
-import ConnectedLayout from '../Layout/ConnectedLayout';
 import WhiteRabbit from './WhiteRabbit';
 import NotFoundPage from './404';
+import SecurityLayout from '../Layout/SecurityLayout';
+import ConnectedLayout from '../Layout/ConnectedLayout';
+import AppLayout from '../Layout/AppLayout';
+
 import './css/App.css';
 
+import AppCR from '../apps/ClashRoyale';
 
 function App() {
   const [user, setUser] = useState(null);
@@ -23,12 +27,15 @@ function App() {
     <div className='app-wrapper'>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Landing />} />
-          <Route path='/security' element={<SecurityLayout user={user} />}>
+          <Route path='/' element={<Landing />}/>
+          <Route path='/security' element={<SecurityLayout user={user}/>}>
             <Route path='login' element={<Terrier />} />
           </Route>
-          <Route path='/' element={<ConnectedLayout user={user} />}>
-            <Route path='white-rabbit' element={<WhiteRabbit />} />
+          <Route path='/' element={<ConnectedLayout user={user}/>}>
+            <Route path='white-rabbit' element={<WhiteRabbit />}/>
+          </Route>
+          <Route path='/app' element={<AppLayout user={user} />}>
+            <Route path='clash-royale' element={<AppCR />}/>
           </Route>
           <Route path='*' element={<NotFoundPage />} />
         </Routes>
