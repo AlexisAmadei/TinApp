671197ccdc47ee28a1a49eb39cc67c4cb755529c
Alexis Amadei <alexisamadei@gmail.com>
2023-09-27 01:48:42 +0300

    [EDIT] minor changes on the cr App view and add cr id

--- src/apps/ClashRoyale.js
+++ src/apps/ClashRoyale.js
@@ -1,22 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { Container } from "@mui/material";
-import './css/ClashRoyale.css';
+import EditIcon from '@mui/icons-material/Edit';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 import { db } from '../.config/firebaseConfig'
-import { doc, updateDoc } from "firebase/firestore";
+import { doc, query, updateDoc, getDoc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+
+import './css/ClashRoyale.css';
 
 export default function AppCR() {
   const user = getAuth().currentUser;
   const [userCR, setUserCR] = useState("");
-  const [getStarted, setGetStarted] = useState(false);
+  const [edit, setEdit] = useState(false);
+  const [inputValue, setInputValue] = useState("");
 
-  async function setupUser() {
-    const userRef = doc(db, "users", user.uid);
-    await updateDoc(userRef, {
-      clashRoyale: userCR
-    })
+  useEffect(() => {
+    async function getCurrentID() {
+      const userRef = doc(db, "users", user.uid);
+      const querySnapshot = await getDoc(userRef);
+      setUserCR(querySnapshot.data().clashRoyale);
+      console.log("db id", userCR);
+    }
+    getCurrentID();
+  }, [user.uid]);
+
+  useEffect(() => {
+    async function updateID() {
+      const userRef = doc(db, "users", user.uid);
+      await updateDoc(userRef, {
+        clashRoyale: userCR,
+      });
+    }
+    updateID();
+  }, [userCR]);
+
+  function changeID() {
+    console.log("input", inputValue)
+    setUserCR(inputValue);
+    setEdit(false);
   }
 
   return (
@@ -25,9 +48,26 @@ export default function AppCR() {
         <h1>Clash Royale Companion</h1>
         <Container>
           <div id="get-started">
-            <button onClick={() => setGetStarted(true)}>Commencer</button>
-            {getStarted && (
-              <div>
+            {!edit && (
+              <div id="savedID">
+                <p>Current save ID: <span>{userCR}</span></p>
+                <EditIcon onClick={() => {
+                  setEdit(true);
+                }} />
+              </div>
+            )}
+            {edit && (
+              <div id="editID">
+                <p>{"Edit your ID :"}</p>
+                  <input
+                    type="text"
+                    placeholder="Enter your ID"
+                    value={inputValue}
+                    onChange={(e) => {
+                      setInputValue(e.target.value);
+                    }}
+                  />
+                <CheckCircleIcon onClick={changeID} />
               </div>
             )}
           </div>
