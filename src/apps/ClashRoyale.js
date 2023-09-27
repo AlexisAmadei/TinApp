140b8fb0118119de97a9b311f58f2fd7683eb8b9
Alexis Amadei <alexisamadei@gmail.com>
2023-09-27 04:04:23 +0300

    [EDIT] new features: player stats functionnal

--- src/apps/ClashRoyale.js
+++ src/apps/ClashRoyale.js
@@ -16,6 +16,14 @@ export default function AppCR() {
   const [userCR, setUserCR] = useState("");
   const [edit, setEdit] = useState(false);
   const [inputValue, setInputValue] = useState("");
+  const [stats, setStats] = useState({
+    pseudo: "",
+    trophies: "",
+    level: "",
+    wins: "",
+    losses: "",
+    draws: "",
+  });
 
   useEffect(() => {
     async function getUserData() {
@@ -60,8 +68,9 @@ export default function AppCR() {
   async function getStats() {
     if (!userCR) return;
     const url = `https://proxy.royaleapi.dev/v1/players/%23${userCR}`;
+    const api_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjliYzhjYTIwLTRlY2ItNDdkOC05MjI0LTU0NjgzMGY0NjA1MSIsImlhdCI6MTY5NTc3MTg3Mywic3ViIjoiZGV2ZWxvcGVyL2U4YzEzNDQzLTc0MDktMTYyNC1kNWNmLTZjZTI5OWQ0NGQ3MiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI0NS43OS4yMTguNzkiXSwidHlwZSI6ImNsaWVudCJ9XX0.AzxWS4CKqNAD2aSlACOHuvFqqAHrAFrzBYM-Gqai_C9xx5sijxQCOTCAloeWiFMD-aFZE-HxEPFNfhaLE96vhw"
     var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjliYzhjYTIwLTRlY2ItNDdkOC05MjI0LTU0NjgzMGY0NjA1MSIsImlhdCI6MTY5NTc3MTg3Mywic3ViIjoiZGV2ZWxvcGVyL2U4YzEzNDQzLTc0MDktMTYyNC1kNWNmLTZjZTI5OWQ0NGQ3MiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI0NS43OS4yMTguNzkiXSwidHlwZSI6ImNsaWVudCJ9XX0.AzxWS4CKqNAD2aSlACOHuvFqqAHrAFrzBYM-Gqai_C9xx5sijxQCOTCAloeWiFMD-aFZE-HxEPFNfhaLE96vhw");
+    myHeaders.append("Authorization", `Bearer ${api_key}`);
 
     var requestOptions = {
       method: 'GET',
@@ -69,14 +78,30 @@ export default function AppCR() {
       redirect: 'follow'
     };
     fetch(url, requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
+      .then(response => response.json())
+      .then(result => parseStats(result))
       .catch(error => console.log('error', error));
   }
 
   useEffect(() => {
     getStats();
-  });
+  }, [userCR]);
+
+  function parseStats(stats) {
+    const { name, trophies, expLevel, wins, losses, battleCount, threeCrownWins, totalDonations, role, clan } = stats;
+    setStats({
+      pseudo: name,
+      trophies: trophies,
+      level: expLevel,
+      wins: wins,
+      losses: losses,
+      total: battleCount,
+      perfectWins: threeCrownWins,
+      donations: totalDonations,
+      role: role,
+      clan: clan.name,
+    });
+  }
 
   return (
     <div className="cr-app-wrapper">
@@ -107,12 +132,25 @@ export default function AppCR() {
               </div>
             )}
           </div>
-          <div id="stats">
-            <div id="stats-header">
-              <RefreshIcon onClick={getStats} />
-              <h3>Your stats: </h3>
+          <div className="stats">
+            <h3>Your stats: </h3>
+            <div className="container-stats">
+              <p>Pseudo: {stats.pseudo}</p>
+              <p>Trophies: {stats.trophies}</p>
+              <p>Max trophies: {stats.maxTrophies}</p>
+              <p>Account Level: {stats.level}</p>
+              <p>Total battles: {stats.total}</p>
+              <p>Wins: {stats.wins}</p>
+              <p>Losses: {stats.losses}</p>
+              <p>Perfect win: { }</p>
             </div>
-            <p>{"json"}</p>
+          </div>
+          <div className="stats">
+            <h3>Clan stats: </h3>
+            <div className="container-stats">
+              <p>Clan Name: {stats.clan}</p>
+              <p>Role: {stats.role}</p>
+            </div>
           </div>
         </Container>
       </div>
